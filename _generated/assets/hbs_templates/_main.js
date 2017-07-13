e3bb7a0f3c34964861b076a60759173da948d5fa
Danny <dannydelgrosso@gmail.com>
2017-07-12 21:55:19 -0400

    Created way for admin to add events to calendar and for it to be displayed on main site

--- _generated/assets/hbs_templates/_main.js
+++ _generated/assets/hbs_templates/_main.js
@@ -1,7 +1,19 @@
-define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper;
+define(["handlebars"], function(Handlebars) { return Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
+    var alias1=container.lambda, alias2=container.escapeExpression;
+
+  return "          <li>"
+    + alias2(alias1((depth0 != null ? depth0.title : depth0), depth0))
+    + "</li>\r\n          <li>"
+    + alias2(alias1((depth0 != null ? depth0.description : depth0), depth0))
+    + "</li>\r\n          <li>"
+    + alias2(alias1((depth0 != null ? depth0.team : depth0), depth0))
+    + "</li>\r\n          <hr>\r\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
   return "<div class=\"mdl-grid\">\r\n  <div class=\"mdl-cell mdl-cell--8-col\">\r\n    <div class=\"main-card mdl-card mdl-shadow--2dp\">\r\n      <div class=\"mdl-card__title\">\r\n        <h2 class=\"mdl-card__title-text\">"
-    + container.escapeExpression(((helper = (helper = helpers.main_title || (depth0 != null ? depth0.main_title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"main_title","hash":{},"data":data}) : helper)))
-    + "</h2>\r\n      </div>\r\n      <div class=\"mdl-card__supporting-text\">\r\n        lorem ipsum\r\n      </div>\r\n      <div class=\"mdl-card__actions mdl-card--border\">\r\n        <a class=\"mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect\">\r\n      Get Started\r\n    </a>\r\n      </div>\r\n      <div class=\"mdl-card__menu\">\r\n        <button class=\"mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect\">\r\n      <i class=\"material-icons\">share</i>\r\n    </button>\r\n      </div>\r\n    </div>\r\n  </div>\r\n  <div class=\"mdl-cell mdl-cell--4-col\">\r\n    <div class=\"twitter-card mdl-card mdl-shadow--2dp\">\r\n      <div class=\"mdl-card__title\">\r\n        <h2 class=\"mdl-card__title-text\">Twitter</h2>\r\n      </div>\r\n    </div>\r\n  </div>\r\n\r\n</div>";
+    + container.escapeExpression(((helper = (helper = helpers.main_title || (depth0 != null ? depth0.main_title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"main_title","hash":{},"data":data}) : helper)))
+    + "</h2>\r\n      </div>\r\n      <div class=\"mdl-card__supporting-text\">\r\n        lorem ipsum\r\n      </div>\r\n      <div class=\"mdl-card__actions mdl-card--border\">\r\n        <h4>Calendar</h2>\r\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.Upcoming_Events : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "      </div>\r\n      <div class=\"mdl-card__menu\">\r\n        <button class=\"mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect\">\r\n      <i class=\"material-icons\">share</i>\r\n    </button>\r\n      </div>\r\n    </div>\r\n  </div>\r\n  <div class=\"mdl-cell mdl-cell--4-col\">\r\n    <div class=\"twitter-card mdl-card mdl-shadow--2dp\">\r\n      <div class=\"mdl-card__title\">\r\n        <h2 class=\"mdl-card__title-text\">Twitter</h2>\r\n      </div>\r\n    </div>\r\n  </div>\r\n\r\n</div>";
 },"useData":true}); });
--- cms/index.js
+++ cms/index.js
@@ -1,28 +1,57 @@
 {
 	application_name: 'IUP Ultimate Frisbee',
-	main_title:'Welcome to the IUP Ultimate Frisbee Page!',
-	tab_names:{
-		tab1:'IUP Ultimate Frisbee',
-		tab2:'The Guys',
-		tab3:'The Girls',
-		tab4:'Join Our Team'
+	main_title: 'Welcome to the IUP Ultimate Frisbee Page!',
+	tab_names: {
+		tab1: 'IUP Ultimate Frisbee',
+		tab2: 'The Guys',
+		tab3: 'The Girls',
+		tab4: 'Join Our Team'
+	},
+	team: {
+		girls: '',
+		guys: '',
+		coed: ''
 	},
 	slide_show: {
-		image1:{
-			$image_type :'image',
-			image:'/assets/img/photo1.jpeg'
+		image1: {
+			$image_type: 'image',
+			image: '/assets/img/photo1.jpeg'
 		},
-		image2:{
-			$image_type :'image',
-			image:'/assets/img/photo2.jpeg'
+		image2: {
+			$image_type: 'image',
+			image: '/assets/img/photo2.jpeg'
 		},
-		image3:{
-			$image_type :'image',
-			image:'/assets/img/photo3.jpeg'
+		image3: {
+			$image_type: 'image',
+			image: '/assets/img/photo3.jpeg'
 		},
-		image4:{
-			$image_type :'image',
-			image:'/assets/img/photo4.jpeg'
+		image4: {
+			$image_type: 'image',
+			image: '/assets/img/photo4.jpeg'
 		}
-	}
+	},
+	Upcoming_Events: [
+		{
+			title: 'Suh Dude Tourney',
+			description: 'RECYCLE THE STACK!',
+			team: 'Guys',
+			$date_type: 'date',
+			date: 'Wednesday, 05 July, 2017',
+			$time_type: 'time',
+			time: '12:30 AM',
+			$date_value: '2017-07-05',
+			$time_value: '00:30:00'
+		},
+		{
+			title: 'Fighting Squirrels Tournament',
+			description: 'Begin',
+			team: 'Coed',
+			$date_type: 'date',
+			date: 'Monday, 11 September, 2017',
+			$time_type: 'time',
+			time: '10:00 PM',
+			$date_value: '2017-09-11',
+			$time_value: '22:00:00'
+		}
+	]
 }
