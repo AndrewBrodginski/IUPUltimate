a63282da16083405ee137bc8f4605cf995d76bd0
Your Name <you@example.com>
2017-08-13 20:45:52 -0400

    added resource links on main page

--- _generated/assets/hbs_templates/_main.js
+++ _generated/assets/hbs_templates/_main.js
@@ -18,7 +18,17 @@ define(["handlebars"], function(Handlebars) { return Handlebars.template({"1":fu
     + " <br/> Team Playing: "
     + alias2(alias1((depth0 != null ? depth0.team : depth0), depth0))
     + "<br/>\r\n          </div>\r\n        </div>\r\n        <hr/> ";
-},"4":function(container,depth0,helpers,partials,data) {
+},"4":function(container,depth0,helpers,partials,data,blockParams) {
+    var stack1, alias1=container.lambda, alias2=container.escapeExpression;
+
+  return "        <div class=\"link-title\">\r\n         <h4> <a href=\""
+    + alias2(alias1(((stack1 = blockParams[0][0]) != null ? stack1.link : stack1), depth0))
+    + "\">"
+    + alias2(alias1(((stack1 = blockParams[0][0]) != null ? stack1.title : stack1), depth0))
+    + "</a></h3>\r\n        </div>\r\n        <div class=\"link-desc\">\r\n          <span>"
+    + alias2(alias1(((stack1 = blockParams[0][0]) != null ? stack1.description : stack1), depth0))
+    + "</span>\r\n        </div>\r\n       \r\n";
+},"6":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "        <div class=\"mySlides fade\">\r\n          <img src=\""
@@ -26,7 +36,7 @@ define(["handlebars"], function(Handlebars) { return Handlebars.template({"1":fu
     + "\" style=\"width:100%\">\r\n          <div class=\"text\">"
     + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
     + "</div>\r\n        </div>\r\n";
-},"6":function(container,depth0,helpers,partials,data) {
+},"8":function(container,depth0,helpers,partials,data) {
     var helper;
 
   return "        <span class=\"dot\" onclick=\"currentSlide"
@@ -35,7 +45,7 @@ define(["handlebars"], function(Handlebars) { return Handlebars.template({"1":fu
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data,blockParams) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
-  return "<div class=\"mdl-grid\">\r\n  <div class=\"mdl-cell mdl-cell--8-col\">\r\n    <div class=\"main-card mdl-card mdl-shadow--2dp\">\r\n      <div class=\"mdl-card__title\">\r\n        <h2 class=\"mdl-card__title-text\">"
+  return "<div class=\"mdl-grid\">\r\n  <div class=\"mdl-cell mdl-cell--8-col\">\r\n    <div class=\"main-card welcome-card mdl-card mdl-shadow--2dp\">\r\n      <div class=\"mdl-card__title\">\r\n        <h2 class=\"mdl-card__title-text\">"
     + container.escapeExpression(((helper = (helper = helpers.main_title || (depth0 != null ? depth0.main_title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"main_title","hash":{},"data":data,"blockParams":blockParams}) : helper)))
     + "</h2>\r\n      </div>\r\n      <div class=\"mdl-card__supporting-text\">\r\n\r\n      </div>\r\n      <div class=\"mdl-card__actions mdl-card--border\">\r\n        <h5>Interested in joining?</h5> <button id=\"joinBtn\" class=\"mdl-button mdl-js-button mdl-js-ripple-effect js-link\" link=\""
     + ((stack1 = container.invokePartial(partials["_join-page"],depth0,{"name":"_join-page","data":data,"blockParams":blockParams,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
@@ -43,9 +53,11 @@ define(["handlebars"], function(Handlebars) { return Handlebars.template({"1":fu
     + ((stack1 = container.invokePartial(partials["_join-page"],depth0,{"name":"_join-page","data":data,"blockParams":blockParams,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
     + "\" />\r\n        <i class=\"material-icons\">share</i>\r\n        </button>\r\n      </div>\r\n    </div>\r\n  </div>\r\n  <div class=\"mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet\">\r\n    <div class=\"twitter-card mdl-card mdl-shadow--2dp\">\r\n      <div class=\"mdl-card__title\">\r\n        <h2 class=\"mdl-card__title-text\">Calendar Events</h2>\r\n\r\n      </div>\r\n      <div class=\"mdl-card__actions mdl-card--border\"></div>\r\n      <div class=\"events\">\r\n        "
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.Upcoming_Events : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 1, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
-    + "\r\n      </div>\r\n    </div>\r\n  </div>\r\n\r\n  <div class=\"mdl-cell mdl-cell--6-col mdl-cell--6-offset-desktop\">\r\n    <div id=\"slideshow-card\" class=\"main-card mdl-card mdl-shadow--2dp\">\r\n\r\n      <div class=\"mdl-card__title\">\r\n        <h2 class=\"mdl-card__title-text\">Team Slideshow</h2>\r\n      </div>\r\n      <div class=\"mdl-card__actions mdl-card--border\"></div>\r\n\r\n      <div class=\"slideshow-container\">\r\n"
-    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.slide_show : depth0),{"name":"each","hash":{},"fn":container.program(4, data, 0, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
-    + "        <a class=\"prev\" onclick=\"plusSlides(-1)\">&#10094;</a>\r\n        <a class=\"next\" onclick=\"plusSlides(1)\">&#10095;</a>\r\n      </div>\r\n      <br>\r\n\r\n      <div style=\"text-align:center\">\r\n"
+    + "\r\n      </div>\r\n    </div>\r\n  </div>\r\n\r\n<div class=\"mdl-cell mdl-cell--6-col\">\r\n  <div class=\"main-card mdl-card mdl-shadow--2dp\">\r\n    <div class=\"mdl-card__title\">\r\n      <h2 class=\"mdl-card__title-text\">Wanna Learn?</h2>\r\n    </div>\r\n    <div class=\"mdl-card__actions mdl-card--border\"></div>\r\n    <div class=\"links-container\">\r\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.information_links : depth0),{"name":"each","hash":{},"fn":container.program(4, data, 1, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
+    + "    </div>\r\n  </div>\r\n</div>\r\n\r\n  <div class=\"mdl-cell mdl-cell--6-col\">\r\n    <div id=\"slideshow-card\" class=\"main-card mdl-card mdl-shadow--2dp\">\r\n\r\n      <div class=\"mdl-card__title\">\r\n        <h2 class=\"mdl-card__title-text\">Team Slideshow</h2>\r\n      </div>\r\n      <div class=\"mdl-card__actions mdl-card--border\"></div>\r\n\r\n      <div class=\"slideshow-container\">\r\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.slide_show : depth0),{"name":"each","hash":{},"fn":container.program(6, data, 0, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
+    + "        <a class=\"prev\" onclick=\"plusSlides(-1)\">&#10094;</a>\r\n        <a class=\"next\" onclick=\"plusSlides(1)\">&#10095;</a>\r\n      </div>\r\n      <br>\r\n\r\n      <div style=\"text-align:center\">\r\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.slide_show : depth0),{"name":"each","hash":{},"fn":container.program(8, data, 0, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
     + "      </div>\r\n\r\n    </div>\r\n  </div>\r\n</div>\r\n</div>\r\n<script>\r\nvar slideIndex = 0;\r\nshowSlides();\r\n\r\nfunction showSlides() {\r\n    var i;\r\n    var slides = document.getElementsByClassName(\"mySlides\");\r\n    var dots = document.getElementsByClassName(\"dot\");\r\n    for (i = 0; i < slides.length; i++) {\r\n       slides[i].style.display = \"none\";  \r\n    }\r\n    slideIndex++;\r\n    if (slideIndex> slides.length) {slideIndex = 1}    \r\n    for (i = 0; i < dots.length; i++) {\r\n        dots[i].className = dots[i].className.replace(\" active\", \"\");\r\n    }\r\n    slides[slideIndex-1].style.display = \"block\";  \r\n    dots[slideIndex-1].className += \" active\";\r\n    setTimeout(showSlides, 4000); // Change image every 2 seconds\r\n}\r\n</script>";
 },"usePartial":true,"useData":true,"useBlockParams":true}); });
--- cms/index.js
+++ cms/index.js
@@ -34,6 +34,7 @@
 	Upcoming_Events: [
 		{
 			title: 'Suh Dude Tourney',
+			$description_type: 'textarea',
 			description: 'RECYCLE THE STACK!',
 			team: 'Guys',
 			$date_type: 'date',
@@ -45,6 +46,7 @@
 		},
 		{
 			title: 'Test Tourney',
+			$description_type: 'textarea',
 			description: 'Testing',
 			team: 'Guys',
 			$date_type: 'date',
@@ -56,6 +58,7 @@
 		},
 		{
 			title: 'Fighting Squirrels Tournament',
+			$description_type: 'textarea',
 			description: 'Begin',
 			team: 'Guys',
 			$date_type: 'date',
@@ -67,6 +70,7 @@
 		},
 		{
 			title: 'Girls Test',
+			$description_type: 'textarea',
 			description: 'Girls Tourney',
 			team: 'Girls',
 			$date_type: 'date',
@@ -75,6 +79,26 @@
 			time: '12:30 AM',
 			$date_value: '2017-08-13',
 			$time_value: '00:30:00'
+		}
+	],
+	information_links: [
+		{
+			title: 'Official Ultimate Frisbee Website',
+			link: 'http://www.usaultimate.org/index.html',
+			$description_type: 'textarea',
+			description: 'This website is the best for all things ultimate! You can sign up here to be able to participate in leagues, learn the rules, and stay up to date on all your favorite teams and official tournaments!'
+		},
+		{
+			title: 'Disc Store',
+			link: 'http://www.usaultimate.org/index.html',
+			$description_type: 'textarea',
+			description: 'Where you can find all the gear! Get anything from official weight ultimate discs, shirts, hats, gloves, and more!'
+		},
+		{
+			title: 'What Is Ultimate.com',
+			link: 'http://www.whatisultimate.com/what-is-ultimate/',
+			$description_type: 'textarea',
+			description: 'Another informational site to tell you exactly what Ultimate Frisbee is and how to play it!'
 		}
 	]
 }
