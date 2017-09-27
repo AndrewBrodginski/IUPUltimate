ef816762bad207a4b154732ac66cb82bc40422df
Your Name <you@example.com>
2017-09-26 20:01:27 -0400

    testing

--- _generated/assets/hbs_templates/_girls_team.js
+++ _generated/assets/hbs_templates/_girls_team.js
@@ -1,3 +1,3 @@
 define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<h1>Girls Page</h1>\r\n\r\n<div class=\"animated slideInUp\">Hello </div>";
+    return "<h1>Girls Page</h1>\r\n\r\n<div class=\"animated slideInUp\">Hello </div>\r\n\r\n";
 },"useData":true}); });
--- _generated/assets/hbs_templates/_guys_team.js
+++ _generated/assets/hbs_templates/_guys_team.js
@@ -1,7 +1,3 @@
 define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper;
-
-  return "<h1>The Guys</h1>\r\n\r\n<h3>"
-    + container.escapeExpression(((helper = (helper = helpers.team_name || (depth0 != null ? depth0.team_name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"team_name","hash":{},"data":data}) : helper)))
-    + "</h3>\r\n\r\n";
+    return "<h1>The Guys</h1>\r\n\r\n";
 },"useData":true}); });
