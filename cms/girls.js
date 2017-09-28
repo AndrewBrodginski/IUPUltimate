8527391895ef01e2217a71bc172d8fe7c4335e8d
Your Name <you@example.com>
2017-09-27 22:42:17 -0400

    almost done with girls and guys pages

---
+++ cms/girls.js
@@ -0,0 +1,60 @@
+{
+	team_picture: {
+		image: '/assets/img/team.jpg',
+		$image_type: 'image',
+		title: 'Team Picture',
+		description: 'Write whatever you want here!'
+	},
+	team_name: 'Irukandji',
+	team: [
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Danielle Siebert',
+			major: 'Math',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Jessica Gates',
+			major: 'Bio?',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Jane Doe',
+			major: 'Psych',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Jane Doe',
+			major: 'Psych',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Jane Doe',
+			major: 'Psych',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Jane Doe',
+			major: 'Psych',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Jane Doe',
+			major: 'Psych',
+			description: 'Put a quote here or a small description!'
+		}
+	]
+}
--- cms/global/global.js
+++ cms/global/global.js
@@ -1,5 +1,5 @@
 {
-		application_name: 'IUP Ultimate Frisbee',
+	application_name: 'IUP Ultimate Frisbee',
 	main_title: 'Welcome to the IUP Ultimate Frisbee Page!',
 	tab_names: {
 		tab1: 'IUP Ultimate Frisbee',
@@ -9,7 +9,6 @@
 	},
 	maincolor: '#800000',
 	$maincolor_type: 'colorpicker',
-
 	Upcoming_Events: [
 		{
 			title: 'Suh Dude Tourney',
@@ -46,6 +45,54 @@
 			time: '10:00 PM',
 			$date_value: '2017-09-11',
 			$time_value: '22:00:00'
+		},
+		{
+			title: 'Girls Test',
+			$description_type: 'textarea',
+			description: 'Girls Tourney',
+			team: 'Girls',
+			$date_type: 'date',
+			date: 'Sunday, 13 August, 2017',
+			$time_type: 'time',
+			time: '12:30 AM',
+			$date_value: '2017-08-13',
+			$time_value: '00:30:00'
+		},
+		{
+			title: 'Girls Test',
+			$description_type: 'textarea',
+			description: 'Girls Tourney',
+			team: 'Girls',
+			$date_type: 'date',
+			date: 'Sunday, 13 August, 2017',
+			$time_type: 'time',
+			time: '12:30 AM',
+			$date_value: '2017-08-13',
+			$time_value: '00:30:00'
+		},
+		{
+			title: 'Girls Test',
+			$description_type: 'textarea',
+			description: 'Girls Tourney',
+			team: 'Girls',
+			$date_type: 'date',
+			date: 'Sunday, 13 August, 2017',
+			$time_type: 'time',
+			time: '12:30 AM',
+			$date_value: '2017-08-13',
+			$time_value: '00:30:00'
+		},
+		{
+			title: 'Girls Test',
+			$description_type: 'textarea',
+			description: 'Girls Tourney',
+			team: 'Girls',
+			$date_type: 'date',
+			date: 'Sunday, 13 August, 2017',
+			$time_type: 'time',
+			time: '12:30 AM',
+			$date_value: '2017-08-13',
+			$time_value: '00:30:00'
 		},
 		{
 			title: 'Girls Test',
