2fa2ea0850fe69f385ac35db9036e13d0081dabe
Your Name <you@example.com>
2017-09-29 15:29:49 -0400

    fixed slow loading issue

--- _generated/assets/hbs_templates/_nav.js
+++ _generated/assets/hbs_templates/_nav.js
@@ -3,7 +3,7 @@ define(["handlebars"], function(Handlebars) { return Handlebars.template({"compi
 
   return "\r\n\r\n<!-- Always shows a header, even in smaller screens. -->\r\n<div class=\"mdl-layout mdl-js-layout mdl-layout--fixed-header\">\r\n  <header class=\"mdl-layout__header\" style=\"background-color:"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.maincolor : stack1), depth0))
-    + "\">\r\n    <div class=\"mdl-layout__header-row\">\r\n      <!-- Title -->\r\n      <a id=\"main-title\" href=\"/\" class=\"mdl-layout-title mdl-navigation__link\">"
+    + ";\">\r\n    <div class=\"mdl-layout__header-row\">\r\n      <!-- Title -->\r\n      <a id=\"main-title\" href=\"/\" class=\"mdl-layout-title mdl-navigation__link\">"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.application_name : stack1), depth0))
     + "</a>\r\n      <!-- Add spacer, to align navigation to the right -->\r\n      <div class=\"mdl-layout-spacer\"></div>\r\n      <!-- Navigation. We hide it in small screens. -->\r\n      <nav class=\"mdl-navigation mdl-layout--large-screen-only\">\r\n        <a class=\"mdl-navigation__link js-link\" href=\"/guys\">"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.tab_names : stack1)) != null ? stack1.tab2 : stack1), depth0))
