679a822376025fc8b11f8b62ec567479fba59137
Your Name <you@example.com>
2017-10-03 21:13:53 -0400

    worked on join page

---
+++ cms/join.js
@@ -0,0 +1,51 @@
+{
+    title_text:'Want to join IUP Ultimate Frisbee?',
+    supporting_text:'Come to a practice or just email one of us below!',
+        girls_schedule:{
+            where:'Fields',
+            day:[
+                {
+                    name:'Monday',
+                    time:'6PM - 8PM'
+                },
+                {
+                    name:'Tuesday',
+                    time:'7PM - 9PM'
+                },
+                {
+                    name:'Thursday',
+                    time:'7PM - 9PM'
+                }
+            ]
+        },
+        guys_schedule:{
+            where:'Fields',
+            day:[
+                {
+                    name:'Monday',
+                    time:'6PM - 8PM'
+                },
+                {
+                    name:'Tuesday',
+                    time:'7PM - 9PM'
+                },
+                {
+                    name:'Thursday',
+                    time:'7PM - 9PM'
+                }
+            ]
+        },
+    girls_contacts:[
+        {
+            name:'',
+            email:''
+        }
+    ],
+    guys_contacts:[
+        {
+            name:'',
+            email:''
+        }
+    ],
+    extra_info:''
+}
