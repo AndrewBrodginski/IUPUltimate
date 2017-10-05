40dbd44914b2af596c5b5a31dcbbaf3c8698e902
Your Name <you@example.com>
2017-10-04 21:19:23 -0400

    just about done with join page

--- cms/join.js
+++ cms/join.js
@@ -1,51 +1,53 @@
 {
-    title_text:'Want to join IUP Ultimate Frisbee?',
-    supporting_text:'Come to a practice or just email one of us below!',
-        girls_schedule:{
-            where:'Fields',
-            day:[
-                {
-                    name:'Monday',
-                    time:'6PM - 8PM'
-                },
-                {
-                    name:'Tuesday',
-                    time:'7PM - 9PM'
-                },
-                {
-                    name:'Thursday',
-                    time:'7PM - 9PM'
-                }
-            ]
-        },
-        guys_schedule:{
-            where:'Fields',
-            day:[
-                {
-                    name:'Monday',
-                    time:'6PM - 8PM'
-                },
-                {
-                    name:'Tuesday',
-                    time:'7PM - 9PM'
-                },
-                {
-                    name:'Thursday',
-                    time:'7PM - 9PM'
-                }
-            ]
-        },
-    girls_contacts:[
-        {
-            name:'',
-            email:''
-        }
-    ],
-    guys_contacts:[
-        {
-            name:'',
-            email:''
-        }
-    ],
-    extra_info:''
+	title_text: 'Want to join IUP Ultimate Frisbee?',
+	supporting_text: 'Come to a practice or just email one of us below!',
+	girls_schedule: {
+		where: 'Fields',
+		day: [
+			{
+				name: 'Monday',
+				time: '6PM - 8PM'
+			},
+			{
+				name: 'Tuesday',
+				time: '7PM - 9PM'
+			},
+			{
+				name: 'Thursday',
+				time: '7PM - 9PM'
+			}
+		]
+	},
+	guys_schedule: {
+		where: 'Fields',
+		day: [
+			{
+				name: 'Monday',
+				time: '6PM - 8PM'
+			},
+			{
+				name: 'Tuesday',
+				time: '7PM - 9PM'
+			},
+			{
+				name: 'Thursday',
+				time: '7PM - 9PM'
+			}
+		]
+	},
+	girls_contacts: [
+		{
+			name: 'Jane Doe',
+			email: 'janedoe@gmail.com'
+		}
+	],
+	guys_contacts: [
+		{
+			name: 'John Doe',
+			email: 'johndoe@gmail.com'
+		}
+	],
+	extra_info: '',
+	join_image: '/remote/direct_uploads/1507166132_iup_join.jpg',
+	$join_image_type: 'image'
 }
