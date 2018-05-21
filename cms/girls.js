fe7e212d7e60244138079a5be7d7819df3883a9d
Your Name <you@example.com>
2018-05-21 19:34:14 -0400

    Changed Team card colors and updated team members

--- cms/girls.js
+++ cms/girls.js
@@ -3,57 +3,50 @@
 		image: '/assets/img/team.jpg',
 		$image_type: 'image',
 		title: 'Team Picture',
-		description: 'Write whatever you want here!'
+		description: 'Girls Ultimate Frisbee Team'
 	},
 	team_name: 'Irukandji',
 	team: [
 		{
 			image: '',
 			$image_type: 'image',
-			name: 'Danielle Siebert',
+			name: 'Member 1',
 			major: 'Math',
 			description: 'Put a quote here or a small description!'
 		},
 		{
 			image: '',
 			$image_type: 'image',
-			name: 'Jessica Gates',
-			major: 'Bio?',
+			name: 'Member 2',
+			major: 'English',
 			description: 'Put a quote here or a small description!'
 		},
 		{
 			image: '',
 			$image_type: 'image',
-			name: 'Jane Doe',
-			major: 'Psych',
+			name: 'Member 3',
+			major: 'Geography',
 			description: 'Put a quote here or a small description!'
 		},
 		{
 			image: '',
 			$image_type: 'image',
-			name: 'Jane Doe',
-			major: 'Psych',
+			name: 'Member 4',
+			major: 'Education',
 			description: 'Put a quote here or a small description!'
 		},
 		{
 			image: '',
 			$image_type: 'image',
-			name: 'Jane Doe',
-			major: 'Psych',
+			name: 'Member 5',
+			major: 'Design',
 			description: 'Put a quote here or a small description!'
 		},
 		{
 			image: '',
 			$image_type: 'image',
-			name: 'Jane Doe',
-			major: 'Psych',
-			description: 'Put a quote here or a small description!'
-		},
-		{
-			image: '',
-			$image_type: 'image',
-			name: 'Jane Doe',
-			major: 'Psych',
+			name: 'Member 6',
+			major: 'Computer Science',
 			description: 'Put a quote here or a small description!'
 		}
 	]
--- cms/guys.js
+++ cms/guys.js
@@ -1,49 +1,53 @@
 {
-    team_picture:{
+	team_picture: {
 		image: '/assets/img/team.jpg',
 		$image_type: 'image',
-		title:'Team Picture',
-		description:'Write whatever you want here!'
-    },
-    team_name:'Kodiaks',
-    team:[
-        {
-            image:'',
-            $image_type: 'image',
-            name:'Danny Del Grosso',
-            major: 'MIS',
-            description: 'Put a quote here or a small description!'
-        },
-        {
-            image:'',
-            $image_type: 'image',
-            name:'Danny Del Grosso',
-            major: 'MIS',
-            description: 'Put a quote here or a small description!'
-        },
-        {
-            image:'',
-            $image_type: 'image',
-            name:'Danny Del Grosso',
-            major: 'MIS',
-            description: 'Put a quote here or a small description!'
-        },
-        {
-            image:'',
-            $image_type: 'image',
-            name:'Danny Del Grosso',
-            major: 'MIS',
-            description: 'Put a quote here or a small description!'
-        },
-        {
-            image:'',
-            $image_type: 'image',
-            name:'Danny Del Grosso',
-            major: 'MIS',
-            description: 'Put a quote here or a small description!'
-        }
-
-    ]
-
-    
+		title: 'Team Picture',
+		description: 'Write whatever you want here!'
+	},
+	team_name: 'Kodiaks',
+	team: [
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Member 1',
+			major: 'Management Information Systems',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Member 2',
+			major: 'Computer Science',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Member 3',
+			major: 'Hospitality',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Member 4',
+			major: 'English',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Member 5',
+			major: 'Economics',
+			description: 'Put a quote here or a small description!'
+		},
+		{
+			image: '',
+			$image_type: 'image',
+			name: 'Member 6',
+			major: 'Accounting',
+			description: 'Put a quote here or a small description!'
+		}
+	]
 }
